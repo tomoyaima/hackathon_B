5d56a717b6bab84b10de48584cccc7ea6221a6ea
今村友哉 <imatomo1234imatomo@gmail.com>
2020-12-01 02:21:05 +0900

    動画再生

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from flask import Flask, render_template, url_for, redirect #追加
+#pip install pillow
+# pip install "pandas<0.25.0"
+# pip install numpy
+
+from flask import Flask, render_template, url_for, redirect, request, Response  #追加
+from camera import VideoCamera 
+from PIL import Image
+import numpy as np
+import cv2
+import io
+
 
 app = Flask(__name__, static_folder='html/static', template_folder='html/templates')
 
@@ -19,9 +29,38 @@ def signup():
 
 @app.route('/index.html')
 def index():
-    #return name
-    return render_template("index.html") #変更
+    return render_template('index.html')
 
+
+@app.route("/img", methods=["POST"])
+def img():
+    #画像処理部分
+    img = request.files["video"].read()
+    
+    # pillow から opencvに変換
+    imgPIL = Image.open(io.BytesIO(img))
+    imgCV = np.asarray(imgPIL)
+
+    imgCV = cv2.bitwise_not(imgCV)
+    cv2.imwrite('./test.jpg', imgCV)
+
+    print("aa")
+    # 好きな処理を入れる
+
+    return "success"
+
+@app.route('/feed')
+def feed():
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def gen():
+    while True:
+        with open('./test.jpg', 'rb') as f:
+            img = f.read()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + img + b'\r\n')
 ## おまじない
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(  threaded=True, debug=True)
+    # ssl_context=('openssl/server.crt', 'openssl/server.key'),
--- html/static/js/index.js
+++ html/static/js/index.js
@@ -42,6 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 });
 
+
 function logout() {
     firebase.auth().signOut().then(() => {
       console.log('ログアウトしました')
@@ -52,4 +53,23 @@ function logout() {
       alert('ログアウト失敗')
     })
 }
+
+
+// function video() {
+//     //動画流す準備
+//     console.log('ログアウトしました')
+//     var video = document.getElementById("video");
+//     // getUserMedia によるカメラ映像の取得
+//     var media = navigator.mediaDevices.getUserMedia({
+//         video: true,//ビデオを取得する
+//         //使うカメラをインカメラか背面カメラかを指定する場合には
+//         //video: { facingMode: "environment" },//背面カメラ
+//         video: { facingMode: "user" },//インカメラ
+//         audio: false,//音声が必要な場合はture
+//     });
+//     //リアルタイムに再生（ストリーミング）させるためにビデオタグに流し込む
+//     media.then((stream) => {
+//         video.srcObject = stream;
+//     });
+// }
 
