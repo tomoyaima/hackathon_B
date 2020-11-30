7a3e8508813b8121344134f0acbbf2bbbc0a740f
numatanumanuma <s2020610@u.tsukuba.ac.jp>
2020-11-30 17:31:28 +0900

    create users.py

---
+++ common/users.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timezone, timedelta
+from common.libs import *
+
+class User:
+    def __init__(self, id):
+        self.id_ = id
+        self.JST_ = timezone(timedelta(hours=+9), 'JST')
+        self.start_t_ = 0
+        self.watching = False
+
+    def start_watching(self):
+        """Userが画面を見始めたらスタート時刻を記録"""
+        if not self.watching:
+            self.start_t_ = datetime.now(self.JST_)
+            self.watching = True
+
+    def end_watching(self):
+        """Userが画面見ていない判定になったら開始時刻と終了時刻をDBへ記録"""
+        if self.watching:
+            self.record_watching_time()
+            self.watching = False
+
+    def record_watching_time(self):
+        document_update(self.id_, self.start_t_, datetime.now(self.JST_))
+
+
+class Users:
+    def __init__(self):
+        self.n = 0
+        self.login_users = {}
+
+    def add_login_user(self, id):
+        """ログインしているUserの追加"""
+        if id not in self.login_users.keys():
+            self.login_users[id] = User(id)
+            self.n += 1
+        else:
+            print('This user is already login!')
+
+    def logout_user(self, id):
+        """ログアウトしたUserの削除"""
+        if id in self.login_users.keys():
+            del(self.login_users[id])
+            self.n += 1
+        else:
+            print('logout error!')
+
+    def get_user(self, id):
+        """idを指定してUserの取得"""
+        if id in self.login_users.keys():
+            return self.login_users[id]
+
--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 """
 テスト用
 """
+from common.users import Users
+import time
 
-from common.libs.firebase import document_set, document_update, collection_get
+# app.pyではusersを宣言
+users = Users()
+# ログインが確認されたらusersのdicにuserを追加
+users.add_login_user('alovelace')
+# userの情報がほしいならget_userする
+user = users.get_user('alovelace')
+# 勉強開始時間を記録
+user.start_watching()
+# 五秒待つ
+time.sleep(5)
+# 勉強が終わったらfirebaseに開始時刻と終了時刻を記録
+user.end_watching()
 
-document_set('alovelace', 'suzukidesu')
-collection_get()
-document_update('alovelace', 'suzukisuzusuzu')
-collection_get()
