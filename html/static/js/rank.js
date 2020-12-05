8a9bf869bbbe09fff0f5b6f38e8a413ebb1102fe
今村友哉 <imatomo1234imatomo@gmail.com>
2020-12-05 23:28:17 +0900

    ランキングの実装修正

--- html/static/js/rank.js
+++ html/static/js/rank.js
@@ -5,7 +5,7 @@ let time =0
 let interval_id =null;
 let table = document.getElementById("container")
 
-let table_tr="<table id = 'container'><tr><th>" + "順位" + "</th><th>"  + "ID" + "</th><th>" + "時間"  + "</th></tr>"
+let table_tr="<table id = 'container'><tr><th>" + "順位" + "</th><th>"  + "名前" + "</th><th>" + "時間(秒)"  + "</th></tr>"
 let i=0
 // let stream = null;
 
@@ -26,34 +26,20 @@ document.addEventListener('DOMContentLoaded', function () {
             user_uid = user.uid
             db.collection("users").orderBy("total_time","desc").get().then((docs) => {
               let time=0;
-             
+              let minute=0;
+              let seconds=0;
               console.log(docs.docs.length)
               docs.forEach(function(doc){
-
                 var data=doc.data();
-                console.log(doc.data())
                 i+=1
-                table_tr+="<tr><td>" + i + "位</td><td>" + data.name + "</td><td>" + data.total_time + "</td></tr>"
-                console.log(doc.data())
-               
-                // for(let i=0;i<data.time.start.length;i++){
-                //   time= data.time.end[i].seconds - data.time.start[i].seconds
-                //   data.total_time += time
-                  
-                // }
-                
+                minute = Math.floor(data.total_time/60.0)
+                seconds = data.total_time%60
+                table_tr+="<tr><td>" + i + "位</td><td>" + data.name + "</td><td>" + minute+"分"+ seconds+"秒"+ "</td></tr>"      
               })
-              
-
+ 
               table_tr+="</table>"
               console.log(table_tr)
               table.innerHTML =table_tr
-              // docs.sort(function(a,b){
-              //   if(a.total_time > b.total_time) return -1;
-              //   if(a.total_time < b.total_time) return 1;
-              //   return 0;
-              // })
-             
         
             }).catch(error => {
                 console.log(error)
