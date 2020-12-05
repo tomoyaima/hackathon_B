b5fc42f511918ef52db9b1cc2cac609e71ff9795
今村友哉 <imatomo1234imatomo@gmail.com>
2020-12-05 14:00:55 +0900

    日付

---
+++ html/static/js/time.js
@@ -0,0 +1,44 @@
+var today = new Date();
+let i = 0
+let render_days = document.getElementById('date0') 
+let day_html =''
+day_html +='<div class="parent">'
+console.log(render_days)
+const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
+const endDate = new Date(year, month,  0) // 月の最後の日を取得
+const endDayCount = endDate.getDate() // 月の末日
+const lastMonthEndDate = new Date(year, month - 1, 0) // 前月の最後の日の情報
+const lastMonthendDayCount = lastMonthEndDate.getDate() // 前月の末日
+const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+
+
+
+for(i =6;i>=0;i--){
+
+    if (w == 0 && d < startDay) {
+        // 1行目で1日の曜日の前
+        let num = lastMonthendDayCount - startDay + d + 1
+        calendarHtml += '<td class="is-disabled">' + num + '</td>'
+    } else if (dayCount > endDayCount) {
+        // 末尾の日数を超えた
+        let num = dayCount - endDayCount
+        calendarHtml += '<td class="is-disabled">' + num + '</td>'
+        dayCount++
+    } else {
+        day_html += "<div class='child'>"+month+"/"+day+"</div>"
+    }
+    
+
+    if (w == 0 && d < startDay) {
+        // 1行目で1日の曜日の前
+        let num = lastMonthendDayCount - startDay + d + 1
+        calendarHtml += '<td class="is-disabled">' + num + '</td>'
+    } 
+        let day = today.getDate() -i
+        let month = today.getMonth() +1;
+        day_html += "<div class='child'>"+month+"/"+day+"</div>"
+}
+day_html +="</div>"
+console.log(render_days);
+
+render_days.innerHTML = day_html
